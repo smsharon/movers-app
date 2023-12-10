b56ac6221d79fcca8a0533401e637a9503e9f942
shallon <shallonmokeira@gmail.com>
2023-12-10 16:32:11 +0300

    modify displayed user details

--- client/src/components/MyProfile.js
+++ client/src/components/MyProfile.js
@@ -1,22 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
-const MyProfile = ({ user }) => {
+const MyProfile = () => {
   const [profileData, setProfileData] = useState(null);
   const [showDetails, setShowDetails] = useState(false);
 
+  // Function to include the access token in requests
+  const includeAccessToken = () => {
+    const token = localStorage.getItem('access_token');
+    return token ? `Bearer ${token}` : '';
+  };
+
   useEffect(() => {
-    // Fetch user profile data based on the user's ID
-    if (user && user.id) {
-      fetch(`http://localhost:5000/users/${user.id}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setProfileData(data); // Set the fetched profile data
-        })
-        .catch((error) => {
-          console.error('Error fetching user profile:', error.message);
-        });
-    }
-  }, [user]);
+    // Fetch user profile data based on the user's role
+    const apiUrl = 'http://localhost:5000/user_profile';
+
+    fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': includeAccessToken(),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProfileData(data); // Set the fetched profile data
+      })
+      .catch((error) => {
+        console.error('Error fetching user profile:', error.message);
+      });
+  }, []);
 
   const toggleDetails = () => {
     setShowDetails(!showDetails);
@@ -25,30 +37,29 @@ const MyProfile = ({ user }) => {
   return (
     <div>
       <h2 onClick={toggleDetails}>My Profile</h2>
-      {showDetails && (
+      {showDetails && profileData && (
         <div>
-          <p>Username: {user && user.username}</p>
-          <p>Email: {user && user.email}</p>
-          <p>Role: {user && user.role}</p>
-          {/* Conditionally render customer or moving company details */}
-          {user && user.role === 'customer' && profileData ? (
+          <p>Username: {profileData.username}</p>
+          <p>Email: {profileData.email}</p>
+          <p>Role: {profileData.role}</p>
+          <p>Profile Completed: {profileData.profile_completed ? 'Yes' : 'No'}</p>
+          {profileData.role === 'customer' && (
             <div>
               <p>Full Name: {profileData.full_name}</p>
               <p>Contact Phone: {profileData.contact_phone}</p>
               <p>Address: {profileData.address}</p>
               <p>Preferred Contact Method: {profileData.preferred_contact_method}</p>
             </div>
-          ) : null}
-          {user && user.role === 'moving_company' && profileData ? (
+          )}
+          {profileData.role === 'moving_company' && (
             <div>
               <p>Company Name: {profileData.company_name}</p>
               <p>Contact Person: {profileData.contact_person}</p>
               <p>Contact Email: {profileData.contact_email}</p>
               <p>Contact Phone: {profileData.contact_phone}</p>
               <p>Extra Services: {profileData.extra_services}</p>
-              {/* Add more details as needed */}
             </div>
-          ) : null}
+          )}
         </div>
       )}
     </div>
